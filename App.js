c03f493843cf17adb49c80988cba0f9e98018361
Datta, Adhokshith <Adhokshith.Datta@anthem.com>
2019-08-19 10:30:55 -0500

    Basic Storybook Project Files

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import StorybookUI from './storybook';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>This is a test</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
+ export default StorybookUI;
